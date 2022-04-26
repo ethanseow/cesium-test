3c59384fd9c4ef1580d6d5560c4b8937aac5613b
ethanseow <eseow6088@bths.edu>
2022-04-26 12:19:44 -0400

    added python scripting functionality

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-/*
-    create express routes
-    simulate a "running" python script that will send "hello world" back to user when you go to specific route
+const serverDebug = require('debug')('server')
+const express = require("express");
+const app = express()
+require('dotenv').config()
+const { spawn } = require('child_process')
+
+const PORT = process.env.PORT
+
+app.get('/',(req,res,next) => {
+    let msg = ''
+    const python = spawn('python',['script.py'])
+
+    python.stdout.on('data',(data) => {
+        msg = data.toString()
+        serverDebug(msg)
+    })
+    python.stderr.on('error',(error)=>{
+        serverDebug(error)
+    })
+    python.on('close',()=>{
+        res.send({message:msg})
+    })
     
-*/
+})
+
+app.listen(PORT, (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log(`Listening on port ${PORT}`)
+    }
+})
